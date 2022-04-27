ef15db292e42ad41c7266793ebc911f91951bc92
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-04-26 17:16:20 -0700

    Clean the sourceFile paths

--- docker/testfixture/lua/includes/dev_server_test.lua
+++ docker/testfixture/lua/includes/dev_server_test.lua
@@ -1,7 +1,28 @@
+local table_concat = table.concat
+local table_insert = table.insert
+local table_Copy = table.Copy
+local string_Split = string.Split
+
 local failures = {}
 
+local function cleanSource( fileName )
+    local spl = string_Split( fileName, "/" )
+    for i, step in ipairs( spl ) do
+        if step == "lua" then
+            return table_concat( spl, "/", i, #spl )
+        end
+    end
+
+    return spl
+end
+
 hook.Add( "GLuaTest_RanTestCase", "TestLog", function( _, _, success, errInfo )
-    if not success then table.insert( failures, errInfo ) end
+    if not success then
+        local errCopy = table_Copy( errInfo )
+        errCopy.locals = nil
+        errCopy.sourceFile = cleanSource( errCopy.sourceFile )
+        table_insert( failures, errInfo )
+    end
 end )
 
 hook.Add( "GLuaTest_RanTestFiles", "TestComplete", function()
