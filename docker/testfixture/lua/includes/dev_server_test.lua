1528a92651c59381e346fe52c23978dba5c0849c
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-04-26 20:20:49 -0700

    Clean up debugging

--- docker/testfixture/lua/includes/dev_server_test.lua
+++ docker/testfixture/lua/includes/dev_server_test.lua
@@ -27,12 +27,11 @@ end )
 
 hook.Add( "GLuaTest_RanTestFiles", "TestComplete", function()
     if #failures > 0 then
-        print( "Test failures detected, writing to log.." )
-        PrintTable( failures )
+        print( tostring( #failures ) .. "test failures detected, writing to log.." )
         file.Write( "gluatest_failures.json", util.TableToJSON( failures ) )
     end
 
-    print("Got GLuaTest TestComplete callback, exiting")
+    print( "Got GLuaTest TestComplete callback, exiting" )
     file.Write( "gluatest_clean_exit.txt", "true" )
     timer.Simple( 1, engine.CloseServer )
 end )
