7ea366ecb4eb7b6045ffb4729c4f62c43d819623
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-04-26 20:30:31 -0700

    Move GH reporting to the testfixture for better context

--- docker/testfixture/lua/includes/dev_server_test.lua
+++ docker/testfixture/lua/includes/dev_server_test.lua
@@ -1,8 +1,10 @@
 local table_concat = table.concat
 local table_insert = table.insert
 local string_Split = string.Split
+local string_format = string.format
 
 local failures = {}
+local ghOutput = CreateConVar( "gluatest_github_output", true, FCVAR_ARCHIVE, "", 0, 1 )
 
 local function cleanSource( fileName )
     local spl = string_Split( fileName, "/" )
@@ -16,12 +18,20 @@ local function cleanSource( fileName )
 end
 
 hook.Add( "GLuaTest_RanTestCase", "TestLog", function( _, _, success, errInfo )
-    if not success then
-        table_insert( failures, {
-            reason = errInfo.reason,
-            lineNumber = errInfo.lineNumber,
-            sourceFile = cleanSource( errInfo.sourceFile )
-        })
+    if success then return end
+    local failInfo = {
+        reason = errInfo.reason,
+        lineNumber = errInfo.lineNumber,
+        sourceFile = cleanSource( errInfo.sourceFile )
+    }
+    table_insert( failures, failInfo )
+
+    -- TODO: Clean this up
+    print( ghOutput:GetBool() )
+    if ghOutput:GetBool() or true then
+        local fi = failInfo
+        local str = "::error file=%s,line=%s::%s"
+        print( string_format( str, fi.sourceFile, fi.lineNumber, fi.reason ) )
     end
 end )
 
