121fc49917b543366c0fe3b4d498c4aeb3672fbd
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-05-24 16:42:02 -0700

    MVP

--- lua/gluatest/expectations.lua
+++ lua/gluatest/expectations.lua
@@ -2,62 +2,64 @@ local type = type
 local IsValid = IsValid
 local string_format = string.format
 
-local function reportFailure( suffix, subject, ... )
-    local fmt = "Expectation Failed: Expected %s " .. suffix
-    local message = string_format( fmt, subject, ... )
-
-    error( message )
-end
-
 local function makeExpectations( subject )
     local expectations = {
         expected = function( suffix, ... )
-            reportFailure( suffix, subject, ... )
+            local fmt = "Expectation Failed: Expected %s " .. suffix
+            local message = string_format( fmt, subject, ... )
+
+            error( message )
         end
     }
 
-    local expected = expectations.expected
+    local i = expectations
 
     function expectations.eq( comparison )
         if subject ~= comparison then
-            expected( "to equal '%s'", comparison )
+            i.expected( "to equal '%s'", comparison )
         end
     end
     expectations.equal = expectations.eq
 
     function expectations.beLessThan( comparison )
         if subject >= comparison then
-            expected( "to be less than '%s'", comparison )
+            i.expected( "to be less than '%s'", comparison )
         end
     end
 
     function expectations.beGreaterThan( comparison )
         if subject <= comparison then
-            expected( "to be greater than '%s'", comparison )
+            i.expected( "to be greater than '%s'", comparison )
         end
     end
 
     function expectations.beTrue()
         if subject ~= true then
-            expected( "to be true" )
+            i.expected( "to be true" )
         end
     end
 
     function expectations.beFalse()
         if subject ~= false then
-            expected( "to be false" )
+            i.expected( "to be false" )
         end
     end
 
     function expectations.beValid()
         if not IsValid( subject ) then
-            expected( "to be valid" )
+            i.expected( "to be valid" )
         end
     end
 
     function expectations.beNil()
         if subject ~= nil then
-            expected( "to be nil" )
+            i.expected( "to be nil" )
+        end
+    end
+
+    function expectations.exist()
+        if subject == nil then
+            i.expected( "to exist, got nil" )
         end
     end
 
@@ -65,7 +67,7 @@ local function makeExpectations( subject )
         local class = type( subject )
 
         if class ~= comparison then
-            expected( "to be a '%s'", comparison )
+            i.expected( "to be a '%s'", comparison )
         end
     end
     expectations.beAn = expectations.beA
@@ -74,7 +76,7 @@ local function makeExpectations( subject )
         local success, err = pcall( subject )
 
         if success == false then
-            expected( "to succeed, got: %s", err )
+            i.expected( "to succeed, got: %s", err )
         end
     end
 
@@ -82,7 +84,7 @@ local function makeExpectations( subject )
         local success = pcall( subject )
 
         if success == true then
-            expected( "to error" )
+            i.expected( "to error" )
         end
     end
 
@@ -90,12 +92,12 @@ local function makeExpectations( subject )
         local success, err = pcall( subject )
 
         if success == true then
-            expected( "to error with '%s'", comparison )
+            i.expected( "to error with '%s'", comparison )
         else
             err = string.Split( err, ": " )[2]
 
             if err ~= comparison then
-                expected( "to error with '%s', got '%s'", comparison, err )
+                i.expected( "to error with '%s', got '%s'", comparison, err )
             end
         end
     end
