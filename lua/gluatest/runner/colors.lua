849eec9681842f20fbba671ecd9e28e3a32887cd
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-05-24 19:59:03 -0700

    Lighten the grey colors to fit more terminals

--- lua/gluatest/runner/colors.lua
+++ lua/gluatest/runner/colors.lua
@@ -1,16 +1,11 @@
 local colors = {
     red = Color( 255, 0, 0 ),
     green = Color( 0, 255, 0 ),
-    grey = Color( 136, 151, 158 ),
-    darkgrey = Color( 85, 85, 85 ),
+    grey = Color( 175, 192, 198 ),
+    darkgrey = Color( 135, 135, 135 ),
     yellow = Color( 235, 226, 52 ),
     white = Color( 220, 220, 220 ),
     blue = Color( 120, 162, 204 )
 }
-
-if CLIENT then
-    -- The default darkgrey is too dark for gmod terminal
-    colors.darkgrey = Color( 125, 125, 125 )
-end
 
 return colors
