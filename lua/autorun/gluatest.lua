d9c2cc6a94de52eb5f3be0190768f9db6ca1b713
Brandon Sturgeon <brandon@brandonsturgeon.com>
2023-02-03 10:25:11 -0800

    Fix cl error

--- lua/autorun/gluatest.lua
+++ lua/autorun/gluatest.lua
@@ -1,1 +1,7 @@
-include( "gluatest/init.lua" )
+if SERVER then
+    include( "gluatest/init.lua" )
+else
+    if file.Exists( "gluatest/init.lua", "LUA" ) then
+        include( "gluatest/init.lua" )
+    end
+end
