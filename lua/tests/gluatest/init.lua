edf5dd21fc6bbf611e7aa5b15d63555a09442357
Brandon Sturgeon <brandon@brandonsturgeon.com>
2024-10-29 17:24:32 -0700

    Add init/colors tests

---
+++ lua/tests/gluatest/init.lua
@@ -0,0 +1,28 @@
+return {
+    groupName = "Initialization",
+
+    cases = {
+        {
+            name = "Table exists",
+            func = function()
+                expect( _G.GLuaTest ).to.exist()
+            end
+        },
+
+        {
+            name = "gluatest_run_tests concommand exists",
+            func = function()
+                local commands = concommand.GetTable()
+                expect( commands["gluatest_run_tests"] ).to.beA( "function" )
+            end
+        },
+
+        {
+            name = "Convars exist",
+            func = function()
+                expect( ConVarExists( "gluatest_use_ansi" ) ).to.beTrue()
+                expect( ConVarExists( "gluatest_enable" ) ).to.beTrue()
+            end
+        }
+    }
+}
