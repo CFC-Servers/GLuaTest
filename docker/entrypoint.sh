539a24f4fb5a0362ec1bb72d496aee059aa93ce0
Raphael <64648134+RaphaelIT7@users.noreply.github.com>
2025-08-19 10:05:13 +0200

    Don't use ::notice:: since it's pointless (#85)
    * Don't use ::notice:: since it's pointless to add a annotation to the workflow run
    
    * Make Lint happy
    
    * Update docker/entrypoint.sh
    
    Co-authored-by: Sarah Sturgeon <sarah@sarahsturgeon.com>
    
    ---------
    
    Co-authored-by: Sarah Sturgeon <sarah@sarahsturgeon.com>

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -167,6 +167,6 @@ if [ -s "$server/data/gluatest_failures.json" ]; then
     echo "::warning:: Test failures detected - Failing workflow"
     exit 1
 else
-    echo "::notice:: No test failures detected"
+    echo "No test failures detected"
     exit 0
 fi
